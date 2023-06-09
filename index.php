0e61a80cb2ee270f2b88927be3023b3c496b0205
HardsSec <134303792+HardsSec@users.noreply.github.com>
2023-06-09 20:36:53 +0100

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,73 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Importance of Cybersecurity</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
 
-echo "Hello Alex!";
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .image-container {
+            width: 100%;
+            margin: 20px 0;
+            text-align: center;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            margin: 20px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>The Importance of Cybersecurity</h1>
+
+        <div class="image-container">
+            <img src="cybersecurity_image1.jpg" alt="Cybersecurity Image 1">
+        </div>
+
+        <p>
+            In today's interconnected world, cybersecurity has become more critical than ever. With the increasing number of cyber threats and data breaches, protecting sensitive information and maintaining the privacy and integrity of digital assets is of utmost importance.
+        </p>
+
+        <div class="image-container">
+            <img src="cybersecurity_image2.jpg" alt="Cybersecurity Image 2">
+        </div>
+
+        <p>
+            Cybersecurity involves implementing measures to prevent unauthorized access, misuse, and damage to computer systems, networks, and data. It encompasses various practices, such as strong password management, regular software updates, secure network configurations, and robust firewalls.
+        </p>
+
+        <div class="image-container">
+            <img src="cybersecurity_image3.jpg" alt="Cybersecurity Image 3">
+        </div>
+
+        <p>
+            The consequences of inadequate cybersecurity can be severe, including financial losses, reputational damage, legal issues, and compromised personal or business information. Cyber attacks can disrupt operations, lead to data loss, and even result in identity theft or fraud.
+        </p>
+
+        <div class="image-container">
+            <img src="cybersecurity_image4.jpg" alt="Cybersecurity Image 4">
+        </div>
+
+        <p>
+            By prioritizing cybersecurity, individuals and organizations can safeguard their digital assets and mitigate potential risks. It is essential to stay updated on the latest threats, educate oneself and employees, and implement robust security measures to protect against cyber threats.
+        </p>
+    </div>
+</body>
+</html>
+
