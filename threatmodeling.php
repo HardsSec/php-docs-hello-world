81c094ec7c810a5d0b5d4a149d02eb94074ba498
HardsSec <134303792+HardsSec@users.noreply.github.com>
2023-06-09 21:02:55 +0100

    Update threatmodeling.php
    Got ChatGPT to redesign

--- threatmodeling.php
+++ threatmodeling.php
@@ -2,23 +2,61 @@
 <html>
 <head>
     <title>Importance of Threat Modeling</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        header {
+            background-color: #f5f5f5;
+            padding: 10px 0;
+        }
+
+        nav {
+            text-align: center;
+        }
+
+        nav a {
+            text-decoration: none;
+            color: #333;
+            margin: 0 10px;
+        }
+
+        nav a:hover {
+            color: #555;
+        }
+    </style>
 </head>
 <body>
     <header>
         <nav>
-            <a href="index.php">Importance of Cybersecurity</a>
+            <a href="index.php">Importance of Cybersecurity</a> |
             <a href="threatmodeling.php">Importance of Threat Modeling</a>
         </nav>
     </header>
-    <h1>The Importance of Threat Modeling</h1>
-    <p>Threat modeling is a crucial process in developing secure systems and applications. It involves identifying potential threats and vulnerabilities, assessing risks, and implementing appropriate countermeasures.</p>
-    <p>By conducting threat modeling, organizations can:</p>
-    <ul>
-        <li>Identify and prioritize potential security risks and vulnerabilities.</li>
-        <li>Understand potential attack vectors and scenarios.</li>
-        <li>Make informed decisions about security controls and mitigations.</li>
-        <li>Optimize resource allocation for security measures.</li>
-        <li>Improve the overall security posture of systems and applications.</li>
-    </ul>
+    <div class="container">
+        <h1>The Importance of Threat Modeling</h1>
+        <p>Threat modeling is a crucial process in developing secure systems and applications. It involves identifying potential threats and vulnerabilities, assessing risks, and implementing appropriate countermeasures.</p>
+        <p>By conducting threat modeling, organizations can:</p>
+        <ul>
+            <li>Identify and prioritize potential security risks and vulnerabilities.</li>
+            <li>Understand potential attack vectors and scenarios.</li>
+            <li>Make informed decisions about security controls and mitigations.</li>
+            <li>Optimize resource allocation for security measures.</li>
+            <li>Improve the overall security posture of systems and applications.</li>
+        </ul>
+    </div>
 </body>
 </html>
